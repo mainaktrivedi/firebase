fb0fc0db731e1c7ad76e4449651d20333f59ae14
Mainak Trivedi <mainak_trivedi@yahoo.com>
2025-01-18 10:13:53 -0800

    Remove constants.js from tracking

--- backend/src/constants.js
+++
@@ -1,7 +0,0 @@
-const WEATHER_API_KEY = "7afa46f2e91768e7eeeb9001ce40de19"
-const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/weather?"
-
-export default {
-    WEATHER_API_KEY,
-    WEATHER_API_URL
-}
